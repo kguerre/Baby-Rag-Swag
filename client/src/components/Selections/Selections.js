2970c66fbed9d591e5af64e034d13edfbba6e68e
kobe <kobe@vps-MacBook-Air.local>
2017-10-07 12:55:13 -0700

    update server port and selections.js

--- client/src/components/Selections/Selections.js
+++ client/src/components/Selections/Selections.js
@@ -1,25 +1,50 @@
-import React, { Component } from "react";
-import { Form } from "semantic-ui-react";
+// import React, { Component } from "react";
+// import { Form } from "semantic-ui-react";
 
 
-class FormExampleSubcomponentControl extends Component {
-  state = {};
+// class FormExampleSubcomponentControl extends Component {
+//   state = {};
 
-  handleChange = (e, { value }) => this.setState({ value });
+//   handleChange = (e, { value }) => this.setState({ value });
 
-  render() {
-    const { value } = this.state;
-    return <Form>
-        <Form.Group inline>
-          <label>Size</label>
-          <Form.Radio label="X-Small" value="xs" checked={value === "xs"} onChange={this.handleChange} />
-          <Form.Radio label="Small" value="sm" checked={value === "sm"} onChange={this.handleChange} />
-          <Form.Radio label="Medium" value="md" checked={value === "md"} onChange={this.handleChange} />
-          <Form.Radio label="Large" value="lg" checked={value === "lg"} onChange={this.handleChange} />
-        </Form.Group>
-        <Form.Button type="submit">Add to Cart</Form.Button>
-      </Form>;
-  }
-}
+//   render() {
+//     const { value } = this.state;
+//     return <Form>
+//         <Form.Group inline>
+//           <label>Size</label>
+//           <Form.Radio label="X-Small" value="xs" checked={value === "xs"} onChange={this.handleChange} />
+//           <Form.Radio label="Small" value="sm" checked={value === "sm"} onChange={this.handleChange} />
+//           <Form.Radio label="Medium" value="md" checked={value === "md"} onChange={this.handleChange} />
+//           <Form.Radio label="Large" value="lg" checked={value === "lg"} onChange={this.handleChange} />
+//         </Form.Group>
+//         <Form.Button type="submit">Add to Cart</Form.Button>
+//       </Form>;
+//   }
+// }
 
-export default FormExampleSubcomponentControl;
+// export default FormExampleSubcomponentControl;
+import React from "react";
+import { Dropdown, Button } from "semantic-ui-react";
+
+
+const DropdownExampleSearchSelectionTwo = () => (
+  <div>
+    <Dropdown
+      placeholder="Size"
+      search
+      selection
+      options={[
+        { key: "XS", value: "X-Small", text: "X-Small" },
+        { key: "SM", value: "Small", text: "Small" },
+        { key: "MD", value: "Medium", text: "Medium" },
+        { key: "L", value: "Large", text: "Large" },
+        { key: "XL", value: "X-Large", text: "X-Large" }
+      ]}
+    />
+    <br/>
+    <br />
+    <Button>Add to Cart</Button>
+  </div>
+);
+
+export default DropdownExampleSearchSelectionTwo;
