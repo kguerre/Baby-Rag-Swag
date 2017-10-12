9f10ae27023b13f6d3b54dc2635647b39519564a
kobe <kobe@vps-macbook-air.ad.medctr.ucla.edu>
2017-10-12 08:35:56 -0700

    still won't deploy

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,11 @@ router.use("/api", apiRoutes);
 // router.use(function(req, res) {
 //   res.sendFile(path.join(__dirname, "../client/public/index.html"));
 // });
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-
 
 module.exports = router;
